eece8c9bfccad96e9a98e3a9bfe9209d4c895422
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-22 21:55:27 -0800

    Estamos probando con video de la api de imdb

--- my-web/src/Components/Programas.jsx
+++ my-web/src/Components/Programas.jsx
@@ -5,7 +5,13 @@ import "../Css/Programas.css"
 
 function Programas (){
     return(
-        <div className="main__programas"></div>
+        <div className="main__programas">
+
+            <div className="programa-peli">
+                <video autoPlay muted src="https://imdb-video.media-imdb.com/vi531039513/1434659607842-pgv4ql-1440787227613.mp4?Expires=1671860614&Signature=kQW2miAmQ0CBnnxo~KTYSB-PmxsnGaayWMtxpQeikflNMi3XyxLBWmhYGIkKNnlPUjxS11G7mPG8nkeji~UvMCmA00R50XDlvZ-VG~DMuWoClfn4eVYepS3~~9L5tHRfetzscscGBRKd2zDH1DuIc6mW-B9d7HIV2wgXKc3h1moMn28bxB~I00mVlaSvqd6lq2mnqktllzkBkHlTOzGOb41-2fIkPX4adJKjJlC4v6xonlW1tawUMFRM7TdInFR0L-4mGhrrL5ihbh~SHP4IA8vGPbVk542HdDOSTIaRZ3gGpI2HoXalXGlEzt56~J5in9F1qNeZoIvekqjMec0Eqg__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA"></video>
+            </div>
+
+        </div>
     );
 }
 
