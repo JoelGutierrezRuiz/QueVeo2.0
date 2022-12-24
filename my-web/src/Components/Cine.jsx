1498a4c300752017e7b859de04d349fab4548164
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-23 22:30:17 -0800

    HEmos mejorado el componente para un futuro solo pasarle los datos

--- my-web/src/Components/Cine.jsx
+++ my-web/src/Components/Cine.jsx
@@ -13,11 +13,23 @@ function Cine (){
 
             <div className="cine-peli">
                 <div className="cine-peli-info">
-                    20:08 - 21:40
+                    <p>20:08 - 21:40</p>
                 </div>
                 <div className="cine-peli-img-container">
                     <div className="cine-peli-img-canal-logo-container">
                         <img className="cine-peli-img-canal-logo" src="https://www.tvguia.es/sites/default/files/logo/energy.png"></img>
+                    </div>
+
+
+                    
+                    <div className="cine-peli-rating-container">
+
+                        <div className="cine-peli-rating-imdbLogo-container">
+                            <img className="cine-peli-rating-imdbLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"></img>
+                        </div>
+
+
+                        <p className="cine-peli-rating-number">5,6</p>
                     </div>
                     
                     <img src="https://m.media-amazon.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_FMjpg_UX1000_.jpg" ></img>
@@ -27,12 +39,25 @@ function Cine (){
 
             <div className="cine-peli">
                 <div className="cine-peli-info">
-                    20:08 - 21:40
+                    <p>20:08 - 21:40</p>
                 </div>
                 <div className="cine-peli-img-container">
                     <div className="cine-peli-img-canal-logo-container">
                         <img className="cine-peli-img-canal-logo" src="https://www.tvguia.es/sites/default/files/logo/la-1.png"></img>
                     </div>
+
+                    <div className="cine-peli-rating-container">
+
+                        <div className="cine-peli-rating-imdbLogo-container">
+                            <img className="cine-peli-rating-imdbLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"></img>
+                        </div>
+
+
+                        <p className="cine-peli-rating-number">8,9</p>
+                    </div>
+
+
+
                     <img src="https://lumiere-a.akamaihd.net/v1/images/image_8b5ca578.jpeg?region=0,0,540,810" ></img>
                 </div>
 
@@ -40,13 +65,26 @@ function Cine (){
 
             <div className="cine-peli">
                 <div className="cine-peli-info">
-                    20:08 - 21:40
+                    <p>20:08 - 21:40</p>
                 </div>
                 
                 <div className="cine-peli-img-container">
                     <div className="cine-peli-img-canal-logo-container">
                         <img className="cine-peli-img-canal-logo" src="https://www.tvguia.es/sites/default/files/logo/dkiss.png"></img>
                     </div>
+
+                    <div className="cine-peli-rating-container">
+
+                        <div className="cine-peli-rating-imdbLogo-container">
+                            <img className="cine-peli-rating-imdbLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"></img>
+                        </div>
+
+
+                        <p className="cine-peli-rating-number">9,6</p>
+                    </div>
+
+
+
                     <img src="https://m.media-amazon.com/images/M/MV5BOTgxMDQwMDk0OF5BMl5BanBnXkFtZTgwNjU5OTg2NDE@._V1_FMjpg_UX1000_.jpg" ></img> 
                 </div>
 
@@ -54,12 +92,21 @@ function Cine (){
 
             <div className="cine-peli">
                 <div className="cine-peli-info">
-                    20:08 - 21:40
+                    <p>20:08 - 21:40</p>
                 </div>
                 
                 <div className="cine-peli-img-container">
                     <div className="cine-peli-img-canal-logo-container">
                         <img className="cine-peli-img-canal-logo" src="https://www.tvguia.es/sites/default/files/logo/fdf.png"></img>
+                    </div>
+                    <div className="cine-peli-rating-container">
+
+                        <div className="cine-peli-rating-imdbLogo-container">
+                            <img className="cine-peli-rating-imdbLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"></img>
+                        </div>
+                        
+
+                        <p className="cine-peli-rating-number">3,6</p>
                     </div>
                     <img src="https://www.universalpictures-latam.com/tl_files/content/movies/puss_in_boots_2/posters/01.jpg" ></img>
                 </div>
@@ -68,13 +115,25 @@ function Cine (){
 
             <div className="cine-peli">
                 <div className="cine-peli-info">
-                    20:08 - 21:40
+                    <p>20:08 - 21:40</p>
                 </div>
 
                 <div className="cine-peli-img-container">
                     <div className="cine-peli-img-canal-logo-container">
                         <img className="cine-peli-img-canal-logo" src="https://www.tvguia.es/sites/default/files/logo/paramount-network.png"></img>
                     </div>
+
+                    <div className="cine-peli-rating-container">
+
+                        <div className="cine-peli-rating-imdbLogo-container">
+                            <img className="cine-peli-rating-imdbLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"></img>
+                        </div>
+
+
+                        <p className="cine-peli-rating-number">9,0</p>
+                    </div>
+
+
                     <img src="https://m.media-amazon.com/images/M/MV5BMTk3NDE2NzI4NF5BMl5BanBnXkFtZTgwNzE1MzEyMTE@._V1_FMjpg_UX1000_.jpg" ></img>
                 </div>
 
@@ -82,13 +141,25 @@ function Cine (){
 
             <div className="cine-peli">
                 <div className="cine-peli-info">
-                    20:08 - 21:40
+                    <p>20:08 - 21:40</p>
+                    
                 </div>
 
                 <div className="cine-peli-img-container">
                     <div className="cine-peli-img-canal-logo-container">
                         <img className="cine-peli-img-canal-logo" src="https://www.tvguia.es/sites/default/files/logo/antena-3.png"></img>
                     </div>
+
+                    <div className="cine-peli-rating-container">
+
+                        <div className="cine-peli-rating-imdbLogo-container">
+                            <img className="cine-peli-rating-imdbLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png"></img>
+                        </div>
+
+
+                        <p className="cine-peli-rating-number">6,0</p>
+                    </div>
+                    
                     <img src="https://m.media-amazon.com/images/M/MV5BMTk5MDExMDg3N15BMl5BanBnXkFtZTgwODkzMTk1NjE@._V1_.jpg" ></img>
                 </div>
 
