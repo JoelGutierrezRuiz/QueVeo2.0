d6e53f7fe639e808a9520fc7ec19c6d9e369e6b2
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-24 00:06:39 -0800

    ya se puede buscar el canal falta mostrar la info

--- my-app/index.js
+++ my-app/index.js
@@ -261,9 +261,10 @@ App.get("/",cors(), (req,res)=>{
 })
 
 App.get("/canales/:canal",cors(), (req,res)=>{
-    Task.findById("63a6ac07a590bc02b74d10c8",function(err,doc){
+    Task.findById("63a6ac07a590bc02b74d10c8",req.params.canal,function(err,doc){
         console.log(doc);res.send(doc)
     })
+
 
 
 })
