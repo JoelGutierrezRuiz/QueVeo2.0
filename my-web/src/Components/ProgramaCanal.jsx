0f3f39386ba60b9cac9caf381e56af99c250cf81
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-27 18:26:18 -0800

    Estamos implementando la busqueda de una sola conexion a la apicon imdb

--- my-web/src/Components/ProgramaCanal.jsx
+++ my-web/src/Components/ProgramaCanal.jsx
@@ -1,38 +1,62 @@
 //imports
 import React from "react";
+import { useState } from "react";
 import { useRef } from "react";
 import "../Css/ProgramaCanal.css"
+import {AiOutlineClose} from 'react-icons/ai'
 //imports
 
 function ProgramaCanal (props) {
 
     const element = useRef(null)
+    const [infoContainer,setInfoContainer] = useState(false);
 
     return(
-        
-        <div className="main__programa">
+    <>  
+        {
+        infoContainer?
+            <div className="main__infoPrograma">
+                
+                <div className="infoPrograma-container">
+                    <AiOutlineClose className="infoPrograma-icon" onClick={()=>{setInfoContainer(false)}}  />
+                    <p className="infoPrograma-title">{props.title}</p>
 
-        <div className="programa-info">
-            <p>{props.time}</p>
-        </div>
+                    <div className="infoPrograma-img-container">
+                        <img style={!props.img?{"display":"none"}:null} className="infoPrograma-img"  ref={element} src={props.img} ></img>
+                    </div>
+                    
+                </div>
 
-        <div className="programa-container">
-
-            <p className="programa-titulo">
-                {props.title}
-            </p>
+            </div>
+        :null
+        }
 
 
-            <div className="programa-canal-logo-container">
-                <img className="programa-canal-logo" src={props.logo}></img>
+            <div onClick={()=>{setInfoContainer(true)}} className="main__programa">
+
+                <div className="programa-info">
+                    <p>{props.time}</p>
+                </div>
+
+                <div className="programa-container">
+
+                    <p className="programa-titulo">
+                        {props.title}
+                    </p>
+
+
+                    <div className="programa-canal-logo-container">
+                        <img className="programa-canal-logo" src={props.logo}></img>
+                    </div>
+
+                    
+                    <img style={!props.img?{"display":"none"}:null} className="programa-img"  ref={element} src={props.img} ></img>
+                    
+                </div>
+            
             </div>
 
-            {
-            props.img?<img className="programa-img" ref={element} src={props.img} ></img>:null
-            }
-        </div>
-        
-    </div>
+    </>
     )
 }
 
